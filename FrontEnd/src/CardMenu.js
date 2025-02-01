249b4493e18ce54319097d4b7dc8904e73b48ab0
minsurk1 <minsoo030605@gmail.com>
2025-02-01 18:39:22 +0900

    룰페이지 중간 추가 , 카드존에서의 우클릭 이벤트 추가

---
+++ FrontEnd/src/CardMenu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CardMenu.css';
+
+const CardMenu = ({ x, y, onClose }) => {
+  return (
+    <div 
+      className="card-menu" 
+      style={{ 
+        left: `${x}px`, 
+        top: `${y}px`,
+      }}
+    >
+      <button onClick={() => { console.log('동작 1'); onClose(); }}>동작 1</button>
+      <button onClick={() => { console.log('동작 2'); onClose(); }}>동작 2</button>
+      <button onClick={onClose}>닫기</button>
+    </div>
+  );
+};
+
+export default CardMenu;

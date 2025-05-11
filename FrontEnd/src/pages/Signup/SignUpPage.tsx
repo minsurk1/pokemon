7cab06393bafcc004e264b713cdeba81451c6b79
minsoo <minsoo@mac.local>
2025-05-11 18:48:48 +0900

    Update signup page design

--- FrontEnd/src/pages/Signup/SignUpPage.tsx
+++ FrontEnd/src/pages/Signup/SignUpPage.tsx
@@ -1,46 +1,45 @@
-// 회원가입 API 요청 기능 추가
-// 추후에 유효성 검사 기능 추가해야 함
-import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
-import "./SignUpPage.css";
+import React from "react"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import { FaEye, FaEyeSlash } from "react-icons/fa"
+import "./SignUpPage.css"
+import MessageBox from "../../components/common/MessageBox.tsx"
 
 function SignUpPage() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [message, setMessage] = useState("");
-  const [showMessage, setShowMessage] = useState(false);
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [nickname, setNickname] = useState("")
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const [message, setMessage] = useState("")
+  const [showMessage, setShowMessage] = useState(false)
 
   const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+    setShowPassword(!showPassword)
+  }
 
   const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
+    setShowConfirmPassword(!showConfirmPassword)
+  }
 
   const closeMessage = () => {
-    setShowMessage(false);
-    setMessage("");
-  };
+    setShowMessage(false)
+    setMessage("")
+  }
 
   const handleSignUp = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     // 비밀번호 일치 체크
     if (password !== confirmPassword) {
-      setMessage("비밀번호가 일치하지 않습니다.");
-      setShowMessage(true);
-      return;
+      setMessage("비밀번호가 일치하지 않습니다.")
+      setShowMessage(true)
+      return
     }
 
     const data = {
@@ -48,33 +47,34 @@ function SignUpPage() {
       password,
       email,
       nickname,
-    };
+    }
 
     // 상대 경로로 수정: "/api/auth/signup" 사용
     axios
       .post("http://localhost:5001/api/auth/signup", data) // 정확한 경로
       .then((response) => {
-        setMessage(response.data.message);
-        setShowMessage(true);
+        setMessage(response.data.message)
+        setShowMessage(true)
 
         setTimeout(() => {
-          navigate("/"); // 로그인 페이지로 리디렉션
-        }, 2000);
+          navigate("/") // 로그인 페이지로 리디렉션
+        }, 2000)
       })
       .catch((error) => {
-        console.error("회원가입 에러:", error);
+        console.error("회원가입 에러:", error)
         const errorMessage =
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : "회원가입에 실패했습니다."; // 메시지 처리
-        setMessage(errorMessage);
-        setShowMessage(true);
-      });
-  };
+          error.response && error.response.data.message ? error.response.data.message : "회원가입에 실패했습니다." // 메시지 처리
+        setMessage(errorMessage)
+        setShowMessage(true)
+      })
+  }
 
   return (
     <div className="signup-page">
       <h1>회원가입</h1>
+      <div className="signup-main">
+        <button onClick={() => navigate("/")}>로그인 페이지</button>
+      </div>
       <form onSubmit={handleSignUp}>
         <input
           type="text"
@@ -92,9 +92,7 @@ function SignUpPage() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <span onClick={togglePasswordVisibility}>
-            {showPassword ? <FaEyeSlash /> : <FaEye />}
-          </span>
+          <span onClick={togglePasswordVisibility}>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
         </div>
 
         <div className="password-container">
@@ -105,9 +103,7 @@ function SignUpPage() {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
-          <span onClick={toggleConfirmPasswordVisibility}>
-            {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
-          </span>
+          <span onClick={toggleConfirmPasswordVisibility}>{showConfirmPassword ? <FaEyeSlash /> : <FaEye />}</span>
         </div>
 
         <input
@@ -117,27 +113,24 @@ function SignUpPage() {
           value={nickname}
           onChange={(e) => setNickname(e.target.value)}
         />
-        <input
-          type="email"
-          placeholder="이메일"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <input type="email" placeholder="이메일" required value={email} onChange={(e) => setEmail(e.target.value)} />
+
         <button type="submit">가입하기</button>
       </form>
-      <button onClick={() => navigate("/")}>로그인 페이지로 돌아가기</button>
 
       {showMessage && (
-        <div className="message-box">
-          <p>{message}</p>
-          <button className="su-close-button" onClick={closeMessage}>
-            확인
-          </button>
-        </div>
+        <MessageBox
+          bgColor="#e3f2fd"
+          borderColor="#2196f3"
+          textColor="#0d47a1"
+          onClose={closeMessage}
+          closeborderColor="black"
+        >
+          {message}
+        </MessageBox>
       )}
     </div>
-  );
+  )
 }
 
-export default SignUpPage;
+export default SignUpPage

4dfa2524093ecc3b098c0bbe05e3d5189cc3ed30
minsoo <minsoo@mac.local>
2025-07-01 20:32:31 +0900

    Profile code reset

--- FrontEnd/src/pages/Profile/ProfilePage.tsx
+++ FrontEnd/src/pages/Profile/ProfilePage.tsx
@@ -9,11 +9,14 @@
       <div className="enemy-field"></div>
       <div className="horizontal-line"></div> 
       <div className="player-field"></div>
+      <div className="deck-card"></div>
+      <div className="enemy-cost-zone"></div>
+      <div className="player-cost-zone"></div>
       </div>
       <div className="right-container">
         <div className="enemy-info"></div>
-        <div className="event-zone"></div>
-        <div className="player-info"></div>
+        <div className="event-zone1"></div>
+        <div className="player-info1"></div>
       </div>
     </div>
   );

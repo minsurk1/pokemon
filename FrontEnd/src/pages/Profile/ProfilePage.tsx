90da90febc1035ff470ffe59ba6870e90b562fe4
minsoo <minsoo@mac.local>
2025-07-13 18:31:50 +0900

    Profile source code remove

--- FrontEnd/src/pages/Profile/ProfilePage.tsx
+++ FrontEnd/src/pages/Profile/ProfilePage.tsx
@@ -5,20 +5,7 @@
 
   return(
     <div className="profile-page">
-      <div className="field-container">
-      <div className="enemy-field"></div>
-      <div className="horizontal-line"></div> 
-      <div className="player-field"></div>
-      <div className="deck-card"></div>
-      <div className="enemy-cost-zone"></div>
-      <div className="player-cost-zone"></div>
-      </div>
-      <div className="right-container">
-        <div className="enemy-info"></div>
-        <div className="event-zone1"></div>
-        <div className="player-info1"></div>
-      </div>
-    </div>
+     </div>
   );
   }
   export default ProfilePage;

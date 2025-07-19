c04f135c2187e98c55518947748ffd7fa45faf70
KimTaeJung <xownd200225@gmail.com>
2025-07-19 16:29:29 +0900

    Add code at User.ts: versionKey: false

--- BackEnd/src/models/User.ts
+++ BackEnd/src/models/User.ts
@@ -11,14 +11,19 @@ export interface IUser extends Document {
 }
 
 // 사용자 스키마 정의
-const UserSchema: Schema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  nickname: { type: String, required: true },
-  money: { type: Number, default: 1200 }, // 초기 금액 1200원
-  createdAt: { type: Date, default: Date.now },
-});
+const UserSchema: Schema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    nickname: { type: String, required: true },
+    money: { type: Number, default: 1200 }, // 초기 금액 1200원
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    versionKey: false, // __v 필드 제거
+  }
+);
 
 // 모델 생성 및 내보내기
 export default mongoose.model<IUser>("User", UserSchema);

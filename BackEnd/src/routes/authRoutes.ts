3c167848d2b12cd686550f545ffe93179ec7d4be
KimTaeJung <xownd200225@gmail.com>
2025-10-01 14:41:53 +0900

    비밀번호 8자리 입력 생략

--- BackEnd/src/routes/authRoutes.ts
+++ BackEnd/src/routes/authRoutes.ts
@@ -23,9 +23,11 @@ router.post("/signup", async (req: Request, res: Response) => {
       return res.status(400).json({ success: false, message: "모든 필드를 입력해주세요." });
     }
 
+    /* 잠시 생략 */
+    /*
     if (password.length < 8) {
       return res.status(400).json({ success: false, message: "비밀번호는 최소 8자 이상이어야 합니다." });
-    }
+    }*/
 
     // 중복 검사
     if (await User.findOne({ username })) {
